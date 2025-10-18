944557505262de69d3bb0e89ce4532c658273036
Kraken <nnever1@wgu.edu>
2025-10-18 10:22:08 -0400

    Code challenge

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,23 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+# programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+# print(programming_dictionary)
+
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+student_grades={}
+
+for student in student_scores:
+    if student_scores[student] > 90 <=100:
+        student_grades[student] ="Outstanding"
+    elif student_scores[student] > 80 <= 90:
+        student_grades[student] ="Exceeds Expectations"
+    elif student_scores[student] > 70 <= 80:
+        student_grades[student] ="Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+print(student_grades)
