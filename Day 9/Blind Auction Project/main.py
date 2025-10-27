064884398bff3b47172d9c1ad62847922492c4f6
Kraken <nnever1@wgu.edu>
2025-10-26 20:19:50 -0400

    Code challenge, secret bid

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,20 @@
-# TODO-1: Ask the user for input
-# TODO-2: Save data into dictionary {name: price}
-# TODO-3: Whether if new bids need to be added
-# TODO-4: Compare bids in dictionary
+bidder_name= input("What is your name?\n")
+bid= int(input("What is your bid: $"))
+bids= {
+    bidder_name: bid,
+}
 
+other_bids = True
 
+while other_bids:
+    add_bid = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+    if add_bid == 'yes':
+        bidder_name = input("What is your name?\n")
+        bid = int(input("What is your bid: $"))
+        bids[bidder_name]=bid
+        print(bids)
+    else:
+        max_bid = max(bids, key=bids.get)
+        if max_bid in bids:
+            print(f"The winner is {max_bid}, with a bid of {bids[max_bid]}")
+        other_bids = False
